416fb88909b56a2c88422d3dcaa534596524040a
Nicolás Améstica Vidal <niamesvi3@gmail.com>
2024-09-24 17:54:37 -0300

    Se arreglan las imágenes del portal

--- src/app/contact/components/content/content.component.ts
+++ src/app/contact/components/content/content.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-content',
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
-export class ContentContactComponent { }
+export class ContentContactComponent {
+
+  public contactForm: FormGroup = this.buildForm();
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  buildForm() {
+    return this.formBuilder.group({
+        name: [ '', [Validators.required] ],
+        email: [ '', [Validators.required, Validators.email] ],
+        phone: [ '', [Validators.required] ],
+        message: [ '', [Validators.required] ],
+    });
+  }
+}
--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ContactPage } from './pages/contact.page';
 import { SharedModule } from '../shared/shared.module';
 import { ContactPageRoutingModule } from './contact-routing.module';
@@ -16,6 +16,7 @@ import { ContentContactComponent } from './components/content/content.component'
     IonicModule,
     ContactPageRoutingModule,
     SharedModule,
+    ReactiveFormsModule
   ],
   declarations: [
     ContactPage,
